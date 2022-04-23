ea8aa6076380f3525272e35ca6f9f10e8dd1d66f
ranaahammed012 <98573219+ranaahammed012@users.noreply.github.com>
2022-04-23 12:14:08 +0600

    upto 42/5 done

--- src/App.js
+++ src/App.js
@@ -1,73 +1,180 @@
-import './App.css';
-import { initializeApp } from 'firebase/app';
-import firebaseConfig from './firebase.config';
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
-import { useState } from 'react';
+import "./App.css";
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "./firebase.config";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { useState } from "react";
 
 const app = initializeApp(firebaseConfig);
 
 function App() {
+  const [newUser, setNewUser] = useState(false);
+
   const [user, setUser] = useState({
     isSignedIn: false,
-    name: '',
-    email: '',
-    photo: ''
-  })
+    name: "",
+    email: "",
+    password: "",
+    photo: "",
+  });
   const provider = new GoogleAuthProvider();
   const handleSignIn = () => {
     const auth = getAuth();
     signInWithPopup(auth, provider)
-    .then(res => {
-      const {displayName, photoURL, email} = res.user;
-      const signedInUser = {
-        isSignedIn: true,
-        name: displayName,
-        email: email,
-        photo: photoURL
-      }
-      setUser(signedInUser);
-     console.log(displayName, email, photoURL);
-    })
-    .catch(err => {
-      console.log(err);
-      console.log(err.message)
-    })
-  }
-
+      .then((res) => {
+        const { displayName, photoURL, email } = res.user;
+        const signedInUser = {
+          isSignedIn: true,
+          name: displayName,
+          email: email,
+          photo: photoURL,
+        };
+        setUser(signedInUser);
+        console.log(displayName, email, photoURL);
+      })
+      .catch((err) => {
+        console.log(err);
+        console.log(err.message);
+      });
+  };
 
   const handleSignOut = () => {
     const auth = getAuth();
     signOut(auth)
-    .then(res => {
-      const signedOutUser = {
-        isSignedIn: false,
-        name: '',
-        email: '',
-        photo: ''
-      }
-      setUser(signedOutUser);
-      console.log(res);
-    })
-    .catch(err => {
-  
-    })
-  }
+      .then((res) => {
+        const signedOutUser = {
+          isSignedIn: false,
+          name: "",
+          email: "",
+          photo: "",
+          error: "",
+          success: false,
+        };
+        setUser(signedOutUser);
+        console.log(res);
+      })
+      .catch((err) => {});
+  };
+
+  const handleBlur = (e) => {
+    let isFieldValid = true;
+    if (e.target.name === "email") {
+      isFieldValid = /\S+@\S+\.\S+/.test(e.target.value);
+    }
+    if (e.target.name === "password") {
+      const isPasswordValid = e.target.value.length > 7;
+      const passwordHasNumber = /\d{1}/.test(e.target.value);
+      isFieldValid = isPasswordValid && passwordHasNumber;
+    }
+    if (isFieldValid) {
+      const newUserInfo = { ...user };
+      newUserInfo[e.target.name] = e.target.value;
+      setUser(newUserInfo);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    // console.log(user.email, user.password);
+    if (newUser && user.email && user.password) {
+      const auth = getAuth();
+      createUserWithEmailAndPassword(auth, user.email, user.password)
+        .then((res) => {
+          const newUserInfo = { ...user };
+          newUserInfo.error = "";
+          newUserInfo.success = true;
+          setUser(newUserInfo);
+
+          console.log(res);
+        })
+        .catch((error) => {
+          const newUserInfo = { ...user };
+          newUserInfo.error = error.message;
+          newUserInfo.success = false;
+          setUser(newUserInfo);
+        });
+    }
+
+    if (!newUser && user.email && user.password) {
+      const auth = getAuth();
+      signInWithEmailAndPassword(auth, user.email, user.password)
+        .then(res => {
+          const newUserInfo = { ...user };
+          newUserInfo.error = "";
+          newUserInfo.success = true;
+          setUser(newUserInfo);
+        })
+        .catch((error) => {
+          const newUserInfo = { ...user };
+          newUserInfo.error = error.message;
+          newUserInfo.success = false;
+          setUser(newUserInfo);
+        });
+    }
+
+    e.preventDefault();
+  };
 
   return (
     <div className="App">
-      {
-        user.isSignedIn ? <button onClick={handleSignOut}>Sign Out</button> :
+      {user.isSignedIn ? (
+        <button onClick={handleSignOut}>Sign Out</button>
+      ) : (
         <button onClick={handleSignIn}>Sign In</button>
-      }
-      {
-        user.isSignedIn && 
+      )}
+      {user.isSignedIn && (
         <div>
           <p>Welcome, {user.name}</p>
           <p>Your Email: {user.email}</p>
           <img src={user.photo} alt="" />
         </div>
+      )}
 
-      }
+      <h1>Our Own Authentication</h1>
+      <input
+        type="checkbox"
+        onChange={() => setNewUser(!newUser)}
+        name="newUser"
+      />
+      <label htmlFor="newUser">New User Sign Up</label>
+      <form onSubmit={handleSubmit}>
+        {newUser && (
+          <input
+            name="name"
+            type="text"
+            onBlur={handleBlur}
+            placeholder="Your Name"
+          />
+        )}
+        <br />
+        <input
+          type="text"
+          onBlur={handleBlur}
+          name="email"
+          placeholder="Your Email Address"
+          required
+        />
+        <br />
+        <input
+          type="password"
+          onBlur={handleBlur}
+          name="password"
+          placeholder="Your Password"
+          autoComplete="on"
+          required
+        />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+      <p style={{ color: "red" }}>{user.error}</p>
+      {user.success && (
+      <p style={{ color: "green" }}>User has been {newUser ?"created" : "logged in"} successfully</p>
+      )}
     </div>
   );
 }
